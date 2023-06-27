974785a6e3d4a1fe576d10996e67a8716b00c6c3
Henry Poliquit <paulpoliquit123@gmail.com>
2023-06-27 14:14:04 +0800

    First Commit

---
+++ src/main/java/com/MoW/DEASA/DeaSaApplication.java
@@ -0,0 +1,13 @@
+package com.MoW.DEASA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeaSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeaSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MoW/DEASA/DeaSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoW.DEASA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeaSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
